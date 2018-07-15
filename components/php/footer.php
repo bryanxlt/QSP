bed15102b3ab9b9e86bf3a3391f9c736f246e0bb
Bryan <contact@bryan.lt>
2018-07-15 04:17:31 -0400

    Update footer.php

--- components/php/footer.php
+++ components/php/footer.php
@@ -1,6 +1,6 @@
 <!-- JQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/jq.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
 
 <!-- Popper & Boostrap JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
